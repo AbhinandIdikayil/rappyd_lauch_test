f088a53c895be59a17902c13ee31eb779db5cfb7
Abhinand <abhin4nd6@gmail.com>
2024-12-13 21:31:00 +0530

    fix: typo in vercel file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,44 @@
+import { lazy, Suspense } from "react"
 import { Route, Routes } from "react-router-dom"
 import Login from "./pages/Login/Login"
 import Layout from "./layout/Layout"
 import Home from "./pages/Home/Home"
-import Signup from "./pages/Signup/Signup"
+const Signup  = lazy(() => import("./pages/Signup/Signup")) 
 import ProtectedRoute from "./components/ProtectedRoute"
-import SidebarLayout from "./layout/SidebarLayout"
+import { LoaderIcon } from "lucide-react"
+const SidebarLayout = lazy(() => import('./layout/SidebarLayout'))
 
-function App() {
+function App() { 
 
   return (
-    <Routes>
+    <Suspense fallback={
+        <div className="h-screen flex justify-start items-center bg-background">
+          <LoaderIcon className="animate-spin text-foreground" size={100} />
+        </div>
+      }>
 
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<Home />} />
 
-      <Route path="/" element={<Layout />}>
-        <Route path="" element={<Home />} />
+          <Route path="/login" element={
+            <ProtectedRoute>
+              <Login />
+            </ProtectedRoute>
+          } />
+          <Route path="/signup" element={
+            <ProtectedRoute>
+              <Signup />
+            </ProtectedRoute>
+          } />
 
-        <Route path="/login" element={
-          <ProtectedRoute>
-            <Login />
-          </ProtectedRoute>
-        } />
-        <Route path="/signup" element={
-          <ProtectedRoute>
-            <Signup />
-          </ProtectedRoute>
-        } />
-
-      </Route>
-      <Route path="/dashboard" element={<SidebarLayout />} >
-        <Route path="student" element={<h1> Student </h1>} />
-        <Route path="teacher" element={<h1> Teacher </h1>} />
-      </Route>
-    </Routes>
+        </Route>
+        <Route path="/dashboard" element={<SidebarLayout />} >
+          <Route path="student" element={<h1> Student </h1>} />
+          <Route path="teacher" element={<h1> Teacher </h1>} />
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }
 
